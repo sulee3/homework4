1dec1038ce1af994c1674f1c151c99f57aab3ab0
sulee3 <139187794+sulee3@users.noreply.github.com>
2023-10-25 18:32:08 -0500

    Initial commit

---
+++ interpolators.py
@@ -0,0 +1,6 @@
+import numpy as np
+import scipy as sp
+from scipy.interpolate import interp1d, BarycentricInterpolator
+from scipy.special import roots_chebyt, roots_legendre
+from scipy.integrate import newton_cotes
+import matplotlib.pyplot as plt
